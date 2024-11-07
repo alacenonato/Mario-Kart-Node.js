86d72d3c7b48a8f9ded105bd1976ca6ec6cc1463
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-06 23:32:38 -0300

    Blocks Switch Case

--- src/index.js
+++ src/index.js
@@ -18,10 +18,34 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+
+async function getRandomBlock() {
+    let random = Math.random()
+    let result
+
+    switch (true) {
+        case random < 0.33:
+            result = "Reta"
+            break;
+        case random < 0.66:
+            result = "Curva"
+            break;
+        default:
+            result = "Confronto";
+    }
+    return result;
+}
+
 async function playRaceEngine(character1, character2) {
     for (let round = 1; round <= 5; round++) {
         console.log(`🏁 Rodada ${round}`);
+
+        // sortear bloco
+        let block = await getRandomBlock();
+        console.log(`Bloco: ${block}`);
     }
+
+    
 }
 
 
