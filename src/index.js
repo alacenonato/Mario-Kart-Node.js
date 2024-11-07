83409b95a74dd90bfafcbf2b35bf04527ba058c7
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-07 10:29:08 -0300

    Log Roll Enscapsulate

--- src/index.js
+++ src/index.js
@@ -36,12 +36,18 @@ async function getRandomBlock() {
     return result;
 }
 
+
+async function logRollResult(characterName, block, diceResult, attribute) {
+    console.log(`${characterName} 🎲 rolou um dado de ${block} ${diceResult}`);
+
+}
+
 async function playRaceEngine(character1, character2) {
     for (let round = 1; round <= 5; round++) {
         console.log(`🏁 Rodada ${round}`);
 
         // sortear bloco
-        let block = await getRandomBlock();
+        var block = await getRandomBlock();
         console.log(`Bloco: ${block}`);
     }
     
@@ -56,14 +62,21 @@ async function playRaceEngine(character1, character2) {
     if (block ===    "Reta") {
         TotalTestSkill1 = character1.VELOCIDADE + diceResult1;
         TotalTestSkill2 = character2.VELOCIDADE + diceResult2;
+
+        await logRollResult(character1.NOME, "velocidade", diceResult1, character1.VELOCIDADE);
+        await logRollResult(character2.NOME, "velocidade", diceResult2, character2.VELOCIDADE);
+
     }
     if (block ===    "Curva") {
         TotalTestSkill1 = character1.MANOBRABILIDADE + diceResult1;
         TotalTestSkill2 = character2.MANOBRABILIDADE + diceResult2;
+
+        await logRollResult(character1.NOME, "manobrabilidade", diceResult1, character1.MANOBRABILIDADE);
+        await logRollResult(character2.NOME, "manobrabilidade", diceResult2, character2.MANOBRABILIDADE);
     }
     if (block ===    "Confronto") {
-        TotalTestSkill1 = character1.PODER + diceResult1;
-        TotalTestSkill2 = character2.PODER + diceResult2;
+        let powerResult1 = character1.PODER + diceResult1;
+        let powerResult2 = character2.PODER + diceResult2;
     }
 
 }
